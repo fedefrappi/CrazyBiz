b86e80251097be62f483181478ca166db8bd0b47
matteinn <math.inno@gmail.com>
2011-01-31 21:12:10 +0100

    Added custom search implementation by sql command.

--- CrazyBiz/src/com/example/crazybiz/SearchItem.java
+++ CrazyBiz/src/com/example/crazybiz/SearchItem.java
@@ -15,6 +15,7 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.BaseTheme;
 
 import dao.DBactions;
@@ -26,11 +27,14 @@ public class SearchItem extends VerticalLayout {
 	private Button searchBrand;
 	private Button searchModel;
 	private Button searchStatus;
+	private Button searchCustomQuery;
+	private Window subwindow;
 	private TextField filter;
 	private Table table;
 	private Label selected;
 	private Button backButton;
 	private String queryString;
+	
 	
 	public SearchItem(final CrazybizApplication crazybizApplication, final String username) {
 		this.crazybizApplication = crazybizApplication;
@@ -39,10 +43,28 @@ public class SearchItem extends VerticalLayout {
 		searchOptionsLayout = new HorizontalLayout();
 		searchOptionsLayout.setSpacing(true);
 		filter = new TextField();
-		searchAll = new Button("All");
+		searchAll = new Button("all");
 		searchBrand = new Button("by brand");
 		searchModel = new Button("by model");
 		searchStatus = new Button("by status");
+		searchCustomQuery = new Button("custom");
+		
+		subwindow = new Window("A modal subwindow");
+	    subwindow.setModal(true);
+        // Configure the modal window layout and components
+        VerticalLayout layout = (VerticalLayout) subwindow.getContent();
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        TextField customQuery = new TextField("This is a modal subwindow.");
+        subwindow.addComponent(customQuery);
+        Button closeSubwindow = new Button("Close", new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                (subwindow.getParent()).removeWindow(subwindow);
+            }
+        });
+        layout.addComponent(closeSubwindow);
+        layout.setComponentAlignment(closeSubwindow, Alignment.TOP_RIGHT);
+	    
 		searchAll.addListener(new ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
@@ -86,10 +108,48 @@ public class SearchItem extends VerticalLayout {
 				table.setContainerDataSource(ResultContainer.create(queryString));
 			}
 		});
+		searchCustomQuery.addListener(new ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if(subwindow.getParent()==null){
+					// INIT MODAL WINDOW
+					subwindow = new Window("A modal subwindow");
+					subwindow.setWidth("500px");
+					subwindow.setHeight("300px");
+				    subwindow.setModal(true);
+			        // Configure the modal window layout and components
+			        VerticalLayout layout = (VerticalLayout) subwindow.getContent();
+			        layout.setMargin(true);
+			        layout.setSpacing(true);
+			        final TextField customQuery = new TextField("This is a modal subwindow.");
+			        subwindow.addComponent(customQuery);
+			        Button cancel = new Button("Cancel", new Button.ClickListener() {
+			            public void buttonClick(ClickEvent event) {
+			                (subwindow.getParent()).removeWindow(subwindow);
+			            }
+			        });
+			        Button confirmQuery = new Button("Confirm", new Button.ClickListener() {
+						public void buttonClick(ClickEvent event) {
+							queryString = customQuery.getValue().toString();
+							table.removeAllItems();
+							table.setContainerDataSource(ResultContainer.create(queryString));
+							(subwindow.getParent()).removeWindow(subwindow);
+						}
+					});
+			        HorizontalLayout buttonsLayout = new HorizontalLayout();
+			        buttonsLayout.addComponent(confirmQuery);
+			        buttonsLayout.addComponent(cancel);
+			        layout.addComponent(buttonsLayout);
+			        // SHOW MODAL WINDOW
+					getWindow().addWindow(subwindow);
+				}
+			}
+		});
 		searchOptionsLayout.addComponent(searchAll);
 		searchOptionsLayout.addComponent(searchBrand);
 		searchOptionsLayout.addComponent(searchModel);
 		searchOptionsLayout.addComponent(searchStatus);
+		searchOptionsLayout.addComponent(searchCustomQuery);
 		searchOptionsLayout.addComponent(filter);
 		
 		selected = new Label("");
@@ -98,16 +158,22 @@ public class SearchItem extends VerticalLayout {
 		table.setSelectable(true);
         table.setMultiSelect(false);
         table.setImmediate(true);
-        // turn on column reordering and collapsing
-        table.setColumnReorderingAllowed(true);
-        table.setColumnCollapsingAllowed(true);        
+        table.setColumnReorderingAllowed(false);
+        table.setColumnCollapsingAllowed(true);
         
+        table.addContainerProperty("ItemID", Integer.class, null);
+        table.addContainerProperty("Brand", String.class, null);
+        table.addContainerProperty("Model", String.class, null);
+        table.addContainerProperty("Status", String.class, null);
+                
         // Populate from db with Default query
         queryString = "SELECT item.item_id,brand.brand_name,model.model_name,item.status " +
         		"FROM brand,model,item " +
         		"WHERE item.model_id = model.model_id AND model.brand_id = brand.brand_id";
         table.setContainerDataSource(ResultContainer.create(queryString));
+        table.setVisibleColumns(new String[]{"brand","model","status"});
 
+        
         // Action handler
         table.addListener(new Table.ValueChangeListener() {
 			@Override
@@ -148,8 +214,5 @@ public class SearchItem extends VerticalLayout {
 		this.setComponentAlignment(searchOptionsLayout, Alignment.TOP_CENTER);
 		this.setComponentAlignment(table, Alignment.MIDDLE_CENTER);
 		this.setComponentAlignment(selected, Alignment.MIDDLE_CENTER);
-
-
-
 	}
 }
